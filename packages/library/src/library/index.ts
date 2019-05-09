4ea369c3058e6df01cd5d726358e3541adb15811
Matt Masson <mmasson@microsoft.com>
2019-05-09 17:12:39 -0400

    changed interface

--- packages/library/src/library/index.ts
+++ packages/library/src/library/index.ts
@@ -1,90 +1,74 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 import * as StandardLibrary from "./standard.json";
+import { Export, ExportKind, Module, Signature, Parameter } from "./jsonTypes";
 
-export interface Module {
-    name: string;
-    version: string | null;
+export type Library = {
+    [key: string]: LibraryDefinition;
 }
 
-export enum ExportKind {
-    Constant = "Constant",
-    Constructor = "Constructor",
-    Function = "Function",
-    Type = "Type"
-}
+export type LibraryDefinition = Export;
 
-export interface Export {
-    label: string;
-    kind: ExportKind;
-    summary: string;
-    module: Module;
-    signatures: Signature[] | null;
-}
+// TODO:
+// - Define 'ModuleSets' that reflect the default modules available for a given environment (i.e. Power BI, or the SDK). 
+// - Export LibraryLoader and add constructor that takes in a string[] or ModuleSet
+// - Modules can be added/removed from LibraryLoader
+// - Call LibraryLoader.load() when ready to retrieve definition.
 
-export interface Signature {
-    label: string;
-    documentation: string | null;
-    parameters: Parameter[];
-}
+class LibraryLoader {
+    public static loadAllModules(): Library {
+        return LibraryLoader.loadStandardLibrary();
+    }
 
-export interface Parameter {
-    label: string;
-    documentation: string | null;
-    labelOffsetStart: number;
-    labelOffsetEnd: number;
-}
+    private static loadStandardLibrary(): Library {
+        let library: Library = {};
 
-export type LibraryExports = {
-    [key: string]: Export;
-}
-
-export function loadStandardLibrary(): LibraryExports {
-    let exports: LibraryExports = {};
-
-    // standard library is listed by module
-    StandardLibrary.forEach(m => {
-        const currentModule: Module = {
-            name: m.module,
-            version: m.version
-        };
-
-        m.exports.forEach(e => {
-            const currentExport: Export = {
-                label: e.export,
-                kind: <ExportKind>e.kind,
-                summary: e.summary,
-                module: currentModule,
-                signatures: null
+        // standard library is listed by module
+        StandardLibrary.forEach(m => {
+            const currentModule: Module = {
+                name: m.module,
+                version: m.version
             };
 
-            if (e.signatures !== null) {
-                currentExport.signatures = [];
-                e.signatures.forEach(s => {
-                    const currentSignature: Signature = {
-                        label: s.label,
-                        documentation: s.documentation,
-                        parameters: []
-                    };
+            m.exports.forEach(e => {
+                const currentExport: Export = {
+                    label: e.export,
+                    kind: <ExportKind>e.kind,
+                    summary: e.summary,
+                    module: currentModule,
+                    signatures: null
+                };
 
-                    s.parameters.forEach(p => {
-                        const currentParameter: Parameter = {
-                            label: p.label,
-                            documentation: p.documentation,
-                            labelOffsetStart: p.signatureLabelOffset,
-                            labelOffsetEnd: p.signatureLabelEnd
+                if (e.signatures !== null) {
+                    currentExport.signatures = [];
+                    e.signatures.forEach(s => {
+                        const currentSignature: Signature = {
+                            label: s.label,
+                            documentation: s.documentation,
+                            parameters: []
                         };
 
-                        currentSignature.parameters.push(currentParameter);
+                        s.parameters.forEach(p => {
+                            const currentParameter: Parameter = {
+                                label: p.label,
+                                documentation: p.documentation,
+                                labelOffsetStart: p.signatureLabelOffset,
+                                labelOffsetEnd: p.signatureLabelEnd
+                            };
+
+                            currentSignature.parameters.push(currentParameter);
+                        });
+
+                        currentExport.signatures!.push(currentSignature);
                     });
+                }
 
-                    currentExport.signatures!.push(currentSignature);
-                });
-            }
-
-            exports[currentExport.label] = currentExport;
+                library[currentExport.label] = currentExport;
+            });
         });
-    });
 
-    return exports;
+        return library;
+    }
 }
+
+export const AllModules = LibraryLoader.loadAllModules();
--- packages/library/src/test/simple.ts
+++ packages/library/src/test/simple.ts
@@ -1,26 +1,25 @@
 import "mocha";
 import { expect } from "chai";
-import * as Library from "../library"
+import { AllModules, Library } from "../library";
+
+const PowerQueryLibrary: Library = AllModules;
 
 describe("Library export", () => {
     it("index const by name", () => {
         const val = "BinaryOccurrence.Required";
-        const def = Library.loadStandardLibrary();
-        expect(def).to.exist;
-
-        const ex = def[val];
-        expect(ex.label != null);
+        const def = PowerQueryLibrary[val];
+        expect(def != null, "expected constant wasn't found");
+        expect(def.label).eq(val, "unexpected label");
+        expect(def.summary.length).greaterThan(0, "summary should not be empty");
     });
 
     it("index function by name", () => {
         const val = "List.Distinct";
-        const def = Library.loadStandardLibrary();
-        expect(def).to.exist;
-
-        const ex = def[val];
-        expect(ex.label != null);
-        expect(ex.signatures != null);
-        expect(ex.signatures!.length).eq(2, "expecting 2 signatures");
-        expect(ex.signatures![0].parameters.length).eq(1, "expecting 1 parameter in first signature");
+        const def = PowerQueryLibrary[val];
+        expect(def != null, "expected function wasn't found");
+        expect(def.label != null);
+        expect(def.signatures != null);
+        expect(def.signatures!.length).eq(2, "expecting 2 signatures");
+        expect(def.signatures![0].parameters.length).eq(1, "expecting 1 parameter in first signature");
     });
 });
