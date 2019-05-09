960d236603f5ab42b3ea50b2061935ada7ac9155
Matt Masson <mmasson@microsoft.com>
2019-05-09 15:40:37 -0400

    initial code and tests

--- packages/library/src/library/index.ts
+++ packages/library/src/library/index.ts
@@ -1,6 +1,90 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+import * as StandardLibrary from "./standard.json";
 
-export interface Library {
+export interface Module {
+    name: string;
+    version: string | null;
+}
 
+export enum ExportKind {
+    Constant = "Constant",
+    Constructor = "Constructor",
+    Function = "Function",
+    Type = "Type"
+}
+
+export interface Export {
+    label: string;
+    kind: ExportKind;
+    summary: string;
+    module: Module;
+    signatures: Signature[] | null;
+}
+
+export interface Signature {
+    label: string;
+    documentation: string | null;
+    parameters: Parameter[];
+}
+
+export interface Parameter {
+    label: string;
+    documentation: string | null;
+    labelOffsetStart: number;
+    labelOffsetEnd: number;
+}
+
+export type LibraryExports = {
+    [key: string]: Export;
+}
+
+export function loadStandardLibrary(): LibraryExports {
+    let exports: LibraryExports = {};
+
+    // standard library is listed by module
+    StandardLibrary.forEach(m => {
+        const currentModule: Module = {
+            name: m.module,
+            version: m.version
+        };
+
+        m.exports.forEach(e => {
+            const currentExport: Export = {
+                label: e.export,
+                kind: <ExportKind>e.kind,
+                summary: e.summary,
+                module: currentModule,
+                signatures: null
+            };
+
+            if (e.signatures !== null) {
+                currentExport.signatures = [];
+                e.signatures.forEach(s => {
+                    const currentSignature: Signature = {
+                        label: s.label,
+                        documentation: s.documentation,
+                        parameters: []
+                    };
+
+                    s.parameters.forEach(p => {
+                        const currentParameter: Parameter = {
+                            label: p.label,
+                            documentation: p.documentation,
+                            labelOffsetStart: p.signatureLabelOffset,
+                            labelOffsetEnd: p.signatureLabelEnd
+                        };
+
+                        currentSignature.parameters.push(currentParameter);
+                    });
+
+                    currentExport.signatures!.push(currentSignature);
+                });
+            }
+
+            exports[currentExport.label] = currentExport;
+        });
+    });
+
+    return exports;
 }
---
+++ packages/library/src/test/simple.ts
@@ -0,0 +1,25 @@
+import "mocha";
+import { expect } from "chai";
+import * as Library from "../library"
+
+describe("Library export", () => {
+    it("index const by name", () => {
+        const val = "BinaryOccurrence.Required";
+        const def = Library.loadStandardLibrary();
+        expect(def).to.exist;
+
+        const ex = def[val];
+        expect(ex.label != null);
+    });
+
+    it("index function by name", () => {
+        const val = "List.Distinct";
+        const def = Library.loadStandardLibrary();
+        expect(def).to.exist;
+
+        const ex = def[val];
+        expect(ex.label != null);
+        expect(ex.signatures.length).eq(2, "expecting 2 signatures");
+        expect(ex.signatures[0].parameters.length).eq(1, "expecting 1 parameter in first signature");
+    });
+});
