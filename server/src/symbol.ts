d6ccbcb053661171640f6bca4f29314d39d6ac70
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-06-15 22:52:13 -0700

    simplified constructor

--- server/src/symbol.ts
+++ server/src/symbol.ts
@@ -1,15 +1,9 @@
-import { LineToken } from '@microsoft/powerquery-parser';
-import { LibraryDefinition } from 'powerquery-library';
+import { LineToken } from "@microsoft/powerquery-parser";
+import { LibraryDefinition } from "powerquery-library";
 
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
 export class DocumentSymbol {
-	public readonly token: LineToken;
-	public readonly definition: LibraryDefinition | undefined;
-
-	constructor(token: LineToken, definition: LibraryDefinition | undefined) {
-		this.token = token;
-		this.definition = definition;
-	}
+    constructor(public readonly token: LineToken, public readonly definition: LibraryDefinition | undefined) {}
 }
