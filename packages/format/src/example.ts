fd376c83b3968dce2b3bdd9dfcb74c227fc384c8
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-08-21 14:09:37 -0700

    reverting example

--- packages/format/src/example.ts
+++ packages/format/src/example.ts
@@ -6,8 +6,38 @@ import { Result, ResultKind } from "@microsoft/powerquery-parser";
 import { format, FormatRequest, IndentationLiteral, NewlineLiteral } from "./format";
 import { TFormatError } from "./format/error";
 
-// const text: string = `a = true and b = true and c = true`;
-const text: string = `1 < 2 and 3 > 4 and 5 <> 6 + 7 + 8 * 9`;
+const text: string = `
+// taken from: https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+// removed negative powers, sure to have bugs
+//
+// Function exp_by_squaring(x, n)
+//     if n < 0  then return exp_by_squaring(1 / x, -n);
+//     else if n = 0  then return  1;
+//     else if n = 1  then return  x ;
+//     else if n is even  then return exp_by_squaring(x * x,  n / 2);
+//     else if n is odd  then return x * exp_by_squaring(x * x, (n - 1) / 2);
+let
+    isEven = (x as number) => Number.Mod(x, 2) = 0,
+    pow =
+        (x as number, p as number) =>
+            if p = 0 then
+                1
+            else if p < 0 then
+                error "negative power not supported"
+            else
+                x * @pow(x, p - 1),
+    fastPow =
+        (x as number, p as number) =>
+            if p = 0 then
+                1
+            else if p < 0 then
+                error "negative power not supported"
+            else if isEven(p) then
+                @fastPow(x * x, p / 2)
+            else
+                x * @fastPow(x * x, (p - 1) / 2)
+in
+    fastPow(2, 8)`;
 const request: FormatRequest = {
     text,
     options: {
