039f701cc7ecf14b5260ff7dfc2a19c76b67eeac
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-08-21 13:44:17 -0700

    updating comments

--- packages/format/src/test/serializer/smallPrograms.ts
+++ packages/format/src/test/serializer/smallPrograms.ts
@@ -11,11 +11,11 @@ describe(`small programs`, () => {
 // removed negative powers, sure to have bugs
 //
 // Function exp_by_squaring(x, n)
-//     if n < 0  then return exp_by_squaring(1 / x, -n);
-//     else if n = 0  then return  1;
-//     else if n = 1  then return  x ;
-//     else if n is even  then return exp_by_squaring(x * x,  n / 2);
-//     else if n is odd  then return x * exp_by_squaring(x * x, (n - 1) / 2);
+//     if n < 0 then return exp_by_squaring(1 / x, -n);
+//     else if n = 0 then return  1;
+//     else if n = 1 then return  x ;
+//     else if n is even then return exp_by_squaring(x * x,  n / 2);
+//     else if n is odd then return x * exp_by_squaring(x * x, (n - 1) / 2);
 let
     isEven = (x as number) => Number.Mod(x, 2) = 0,
     pow =
@@ -43,11 +43,11 @@ in
 // removed negative powers, sure to have bugs
 //
 // Function exp_by_squaring(x, n)
-//     if n < 0  then return exp_by_squaring(1 / x, -n);
-//     else if n = 0  then return  1;
-//     else if n = 1  then return  x ;
-//     else if n is even  then return exp_by_squaring(x * x,  n / 2);
-//     else if n is odd  then return x * exp_by_squaring(x * x, (n - 1) / 2);
+//     if n < 0 then return exp_by_squaring(1 / x, -n);
+//     else if n = 0 then return  1;
+//     else if n = 1 then return  x ;
+//     else if n is even then return exp_by_squaring(x * x,  n / 2);
+//     else if n is odd then return x * exp_by_squaring(x * x, (n - 1) / 2);
 let
     isEven = (x as number) => Number.Mod(x, 2) = 0,
     pow =
