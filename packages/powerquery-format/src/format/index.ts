cd61bdee88c7c4f6730ce09243f77407e6d9ab7c
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-04-16 13:58:26 -0700

    moving stuff

--- packages/powerquery-format/src/format/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-import * as FormatError from "./error";
-
-export * from "./format";
-export * from "./serializer";
-export { FormatError };
