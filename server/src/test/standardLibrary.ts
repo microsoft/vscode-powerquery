b710ad217e2f6d1579c37de34fe2b48e5012858a
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-02-22 12:51:17 -0800

    updating language-services and some tests

--- server/src/test/standardLibrary.ts
+++ server/src/test/standardLibrary.ts
@@ -11,7 +11,7 @@ import { Assert } from "@microsoft/powerquery-parser";
 import { expect } from "chai";
 import { MarkupContent, ParameterInformation, SignatureInformation } from "vscode-languageserver";
 
-import { StandardLibrary } from "../library";
+import { StandardLibrary, StandardLibraryDefinitions } from "../library";
 
 function assertGetHover(text: string): Promise<Hover> {
     return createAnalysis(text).getHover();
@@ -26,6 +26,14 @@ async function assertHoverContentEquals(text: string, expected: string): Promise
     const markupContent: MarkupContent = assertAsMarkupContent(hover.contents);
     expect(markupContent.value).to.equal(expected);
 }
+
+const assertIsConstructor: (
+    definition: PQLS.Library.TLibraryDefinition,
+) => asserts definition is PQLS.Library.LibraryConstructor = PQLS.LibraryUtils.assertIsConstructor;
+
+const assertIsFunction: (
+    definition: PQLS.Library.TLibraryDefinition,
+) => asserts definition is PQLS.Library.LibraryFunction = PQLS.LibraryUtils.assertIsFunction;
 
 function assertAsMarkupContent(value: Hover["contents"]): MarkupContent {
     assertIsMarkupContent(value);
@@ -45,41 +53,102 @@ function createAnalysis(textWithPipe: string): PQLS.Analysis {
         line: 0,
     };
 
-    return PQLS.AnalysisUtils.createAnalysis(PQLS.createTextDocument("id", 1, text), position, StandardLibrary);
+    return PQLS.AnalysisUtils.createAnalysis(PQLS.createTextDocument(textWithPipe, 1, text), position, StandardLibrary);
 }
 
 describe(`StandardLibrary`, () => {
-    describe(`getHover`, () => {
-        it(`Table.AddColumn`, async () => {
-            const expression: string = `let foo = Table.AddColumn(1 as table, "bar", each 1) in fo|o`;
-            const expected: string = "[let-variable] foo: table [bar: 1, ...]";
-            await assertHoverContentEquals(expression, expected);
+    describe(`simple`, () => {
+        it("index const by name", () => {
+            const definitionKey: string = "BinaryOccurrence.Required";
+            const maybeLibraryDefinition: PQLS.Library.TLibraryDefinition | undefined = StandardLibraryDefinitions.get(
+                definitionKey,
+            );
+            if (maybeLibraryDefinition === undefined) {
+                throw new Error(`expected constant '${definitionKey}' was not found`);
+            }
+            const libraryDefinition: PQLS.Library.TLibraryDefinition = maybeLibraryDefinition;
+
+            expect(libraryDefinition.label).eq(definitionKey, "unexpected label");
+            expect(libraryDefinition.description.length).greaterThan(0, "summary should not be empty");
+            expect(libraryDefinition.kind).eq(PQLS.Library.LibraryDefinitionKind.Constant);
+            expect(libraryDefinition.primitiveType.kind).eq(PQP.Language.Type.TypeKind.Number);
+        });
+
+        it("index function by name", () => {
+            const exportKey: string = "List.Distinct";
+            const maybeLibraryDefinition: PQLS.Library.TLibraryDefinition | undefined = StandardLibraryDefinitions.get(
+                exportKey,
+            );
+            if (maybeLibraryDefinition === undefined) {
+                throw new Error(`expected constant '${exportKey}' was not found`);
+            }
+            const libraryDefinition: PQLS.Library.TLibraryDefinition = maybeLibraryDefinition;
+            assertIsFunction(libraryDefinition);
+
+            expect(libraryDefinition.label !== null);
+            expect(libraryDefinition.signatures !== null);
+            expect(libraryDefinition.signatures.length).eq(2, "expecting 2 signatures");
+            expect(libraryDefinition.signatures[0].parameters.length).eq(1, "expecting 1 parameter in first signature");
+            expect(libraryDefinition.signatures[0].parameters[0].typeKind).eq(PQP.Language.Type.TypeKind.List);
+        });
+
+        it("#date constructor", () => {
+            const exportKey: string = "#date";
+            const maybeLibraryDefinition: PQLS.Library.TLibraryDefinition | undefined = StandardLibraryDefinitions.get(
+                exportKey,
+            );
+            if (maybeLibraryDefinition === undefined) {
+                throw new Error(`expected constant '${exportKey}' was not found`);
+            }
+            const libraryDefinition: PQLS.Library.TLibraryDefinition = maybeLibraryDefinition;
+            assertIsConstructor(libraryDefinition);
+
+            expect(libraryDefinition.label !== null);
+            expect(libraryDefinition.signatures !== null);
+            expect(libraryDefinition.kind).eq(PQLS.Library.LibraryDefinitionKind.Constructor);
+            expect(libraryDefinition.signatures[0].parameters.length).eq(
+                3,
+                "expecting 3 parameters in first signature",
+            );
+            expect(libraryDefinition.signatures[0].parameters[0].label).eq("year");
+            expect(libraryDefinition.signatures[0].parameters[0].typeKind).eq(PQP.Language.Type.TypeKind.Number);
         });
     });
 
-    it("getSignatureHelp", async () => {
-        const signatureHelp: SignatureHelp = await assertGetSignatureHelp("Table.AddColumn(|");
-        expect(signatureHelp.activeParameter).to.equal(0);
-        expect(signatureHelp.activeSignature).to.equal(0);
-        expect(signatureHelp.signatures.length).to.equal(2);
+    describe(`StandardLibrary `, () => {
+        describe(`Table.AddColumn`, () => {
+            it(`getHover`, async () => {
+                const expression: string = `let foo = Table.AddColumn(1 as table, "bar", each 1) in fo|o`;
+                const expected: string = "[let-variable] foo: table [bar: 1, ...]";
+                await assertHoverContentEquals(expression, expected);
+            });
 
-        const signature: SignatureInformation = PQP.Assert.asDefined(signatureHelp.signatures[0]);
-        Assert.isDefined(signature.documentation);
-        expect(signature.documentation).to.equal(
-            `Adds a column with the specified name. The value is computed using the specified selection function with each row taken as an input.`,
-        );
+            it("getSignatureHelp", async () => {
+                const signatureHelp: SignatureHelp = await assertGetSignatureHelp("Table.AddColumn(|");
 
-        Assert.isDefined(signature.parameters);
-        expect(signature.parameters.length).to.equal(3);
-        const parameters: ReadonlyArray<ParameterInformation> = signature.parameters;
+                expect(signatureHelp.activeParameter).to.equal(0);
+                expect(signatureHelp.activeSignature).to.equal(0);
+                expect(signatureHelp.signatures.length).to.equal(2);
 
-        const firstParameter: ParameterInformation = PQP.Assert.asDefined(parameters[0]);
-        expect(firstParameter.label).to.equal(PQP.Language.Constant.PrimitiveTypeConstantKind.Table);
+                const signature: SignatureInformation = PQP.Assert.asDefined(signatureHelp.signatures[0]);
+                Assert.isDefined(signature.documentation);
+                expect(signature.documentation).to.equal(
+                    `Adds a column with the specified name. The value is computed using the specified selection function with each row taken as an input.`,
+                );
 
-        const secondParameter: ParameterInformation = PQP.Assert.asDefined(parameters[1]);
-        expect(secondParameter.label).to.equal("newColumnName");
+                Assert.isDefined(signature.parameters);
+                expect(signature.parameters.length).to.equal(3);
+                const parameters: ReadonlyArray<ParameterInformation> = signature.parameters;
 
-        const thirdParameter: ParameterInformation = PQP.Assert.asDefined(parameters[2]);
-        expect(thirdParameter.label).to.equal("columnGenerator");
+                const firstParameter: ParameterInformation = PQP.Assert.asDefined(parameters[0]);
+                expect(firstParameter.label).to.equal(PQP.Language.Constant.PrimitiveTypeConstantKind.Table);
+
+                const secondParameter: ParameterInformation = PQP.Assert.asDefined(parameters[1]);
+                expect(secondParameter.label).to.equal("newColumnName");
+
+                const thirdParameter: ParameterInformation = PQP.Assert.asDefined(parameters[2]);
+                expect(thirdParameter.label).to.equal("columnGenerator");
+            });
+        });
     });
 });
