bfa2ea2bd9a24b7eac28ee9bd3e233873536ffb1
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-06-04 13:18:39 -0700

    simplifying SpecializedDuo into SmartTypeResolver

--- server/src/standardLibrary/standardLibraryTypeResolver.ts
+++ server/src/standardLibrary/standardLibraryTypeResolver.ts
@@ -4,55 +4,63 @@
 import * as PQLS from "@microsoft/powerquery-language-services";
 import * as PQP from "@microsoft/powerquery-parser";
 
-import { FunctionName } from "./functionName";
-
 export function createStandardLibraryTypeResolver(
     libraryDefinitions: PQLS.Library.LibraryDefinitions,
 ): PQLS.Inspection.ExternalType.TExternalTypeResolverFn {
     return (request: PQLS.Inspection.ExternalType.TExternalTypeRequest) => {
-        const maybeDuo: SpecializedDuo | undefined = SpecializedDuoResolvers.get(request.identifierLiteral);
-        if (maybeDuo !== undefined) {
-            return resolveSpecializedDuo(request, maybeDuo);
-        } else if (request.kind === PQLS.Inspection.ExternalType.ExternalTypeRequestKind.Value) {
-            return libraryDefinitions.get(request.identifierLiteral)?.asPowerQueryType;
-        } else {
+        const maybeLibraryType: PQP.Language.Type.TPowerQueryType | undefined = libraryDefinitions.get(
+            request.identifierLiteral,
+        )?.asPowerQueryType;
+        if (maybeLibraryType === undefined) {
             return undefined;
+        } else if (request.kind === PQLS.Inspection.ExternalType.ExternalTypeRequestKind.Value) {
+            return maybeLibraryType;
+        }
+        // This scenario shouldn't happen.
+        else {
+            const key: string = PQP.Language.TypeUtils.nameOf(maybeLibraryType);
+            const maybeSmartTypeResolverFn: SmartTypeResolverFn | undefined = SmartTypeResolverFns.get(key);
+
+            if (maybeSmartTypeResolverFn === undefined) {
+                return undefined;
+            }
+
+            const typeChecked: PQP.Language.TypeUtils.CheckedInvocation = PQP.Language.TypeUtils.typeCheckInvocation(
+                request.args,
+                PQP.Language.TypeUtils.assertAsDefinedFunction(maybeLibraryType),
+            );
+
+            return maybeSmartTypeResolverFn(request.args, typeChecked);
         }
     };
 }
 
-function resolveSpecializedDuo(
-    request: PQLS.Inspection.ExternalType.TExternalTypeRequest,
-    specializedDuo: SpecializedDuo,
-): PQP.Language.Type.TPowerQueryType | undefined {
-    switch (request.kind) {
-        case PQLS.Inspection.ExternalType.ExternalTypeRequestKind.Invocation:
-            return specializedDuo.invocationResolverFn(request);
+type SmartTypeResolverFn = (
+    args: ReadonlyArray<PQP.Language.Type.TPowerQueryType>,
+    typeChecked: PQP.Language.TypeUtils.CheckedInvocation,
+) => PQP.Language.Type.TPowerQueryType | undefined;
 
-        case PQLS.Inspection.ExternalType.ExternalTypeRequestKind.Value:
-            return specializedDuo.value;
-
-        default:
-            throw PQP.Assert.isNever(request);
-    }
+function isValidInvocation(typeChecked: PQP.Language.TypeUtils.CheckedInvocation): boolean {
+    return !typeChecked.extraneous.length && !typeChecked.invalid.size && !typeChecked.missing.length;
 }
 
 function resolveTableAddColumn(
-    request: PQLS.Inspection.ExternalType.ExternalInvocationTypeRequest,
-): PQP.Language.Type.TTable | PQP.Language.Type.None | undefined {
+    args: ReadonlyArray<PQP.Language.Type.TPowerQueryType>,
+    typeChecked: PQP.Language.TypeUtils.CheckedInvocation,
+): PQP.Language.Type.TPowerQueryType | undefined {
+    if (!isValidInvocation(typeChecked)) {
+        return PQP.Language.Type.NoneInstance;
+    }
+
     const table: PQP.Language.Type.TPowerQueryType = PQP.Language.TypeUtils.assertAsTable(
-        PQP.Assert.asDefined(request.args[0]),
+        PQP.Assert.asDefined(args[0]),
     );
-    const columnName: PQP.Language.Type.TText = PQP.Language.TypeUtils.assertAsText(
-        PQP.Assert.asDefined(request.args[1]),
-    );
+    const columnName: PQP.Language.Type.TText = PQP.Language.TypeUtils.assertAsText(PQP.Assert.asDefined(args[1]));
     const columnGenerator: PQP.Language.Type.TFunction = PQP.Language.TypeUtils.assertAsFunction(
-        PQP.Assert.asDefined(request.args[2]),
+        PQP.Assert.asDefined(args[2]),
     );
     const maybeColumnType: PQP.Language.Type.TPowerQueryType | undefined =
-        request.args.length === 4
-            ? PQP.Language.TypeUtils.assertAsType(PQP.Assert.asDefined(request.args[3]))
-            : undefined;
+        args.length === 4 ? PQP.Language.TypeUtils.assertAsType(PQP.Assert.asDefined(args[3])) : undefined;
 
     // We can't mutate the given table without being able to resolve columnName to a literal.
     if (!PQP.Language.TypeUtils.isTextLiteral(columnName)) {
@@ -93,48 +101,11 @@ function resolveTableAddColumn(
     }
 }
 
-interface SpecializedDuo {
-    readonly value: PQP.Language.Type.TPowerQueryType;
-    readonly invocationResolverFn: PQLS.Inspection.ExternalType.TExternalInvocationTypeResolverFn;
-}
-
-const TableAddColumnType: PQP.Language.Type.DefinedFunction = PQP.Language.TypeUtils.createDefinedFunction(
-    false,
+// We don't have a way to know when the standard library has a behavioral change.
+// The best we can do is check if a type signature changed by using TypeUtils.nameOf(invoked function).
+const SmartTypeResolverFns: ReadonlyMap<string, SmartTypeResolverFn> = new Map([
     [
-        {
-            isNullable: false,
-            isOptional: false,
-            maybeType: PQP.Language.Type.TypeKind.Table,
-            nameLiteral: "table",
-        },
-        {
-            isNullable: false,
-            isOptional: false,
-            maybeType: PQP.Language.Type.TypeKind.Text,
-            nameLiteral: "newColumnName",
-        },
-        {
-            isNullable: false,
-            isOptional: false,
-            maybeType: PQP.Language.Type.TypeKind.Function,
-            nameLiteral: "columnGenerator",
-        },
-        {
-            isNullable: true,
-            isOptional: true,
-            maybeType: PQP.Language.Type.TypeKind.Type,
-            nameLiteral: "type",
-        },
-    ],
-    PQP.Language.Type.TableInstance,
-);
-
-const SpecializedDuoResolvers: ReadonlyMap<string, SpecializedDuo> = new Map([
-    [
-        FunctionName.TableAddColumn,
-        {
-            value: TableAddColumnType,
-            invocationResolverFn: resolveTableAddColumn,
-        },
+        "(table: table, newColumnName: text, columnGenerator: function, columnType: optional nullable type) => table",
+        resolveTableAddColumn,
     ],
 ]);
