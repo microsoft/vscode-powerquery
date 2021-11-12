9e7f6487b81433b475df69e900fe7e5e604d7f37
Matt Masson <mmasson@microsoft.com>
2021-11-12 09:37:49 -0500

    update packages

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,38 +2,34 @@
 // Licensed under the MIT license.
 
 import * as path from "path";
+// tslint:disable-next-line: no-submodule-imports
+import * as LC from "vscode-languageclient/node";
+
 // tslint:disable-next-line: no-implicit-dependencies
 import { ExtensionContext, workspace } from "vscode";
-import {
-    ForkOptions,
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-    TransportKind,
-} from "vscode-languageclient";
 
-let client: LanguageClient;
+let client: LC.LanguageClient;
 
 export function activate(context: ExtensionContext): void {
     // The server is implemented in node
     const serverModule: string = context.asAbsolutePath(path.join("server", "dist", "server.js"));
     // The debug options for the server
     // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-    const debugOptions: ForkOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+    const debugOptions: LC.ForkOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc },
+    const serverOptions: LC.ServerOptions = {
+        run: { module: serverModule, transport: LC.TransportKind.ipc },
         debug: {
             module: serverModule,
-            transport: TransportKind.ipc,
+            transport: LC.TransportKind.ipc,
             options: debugOptions,
         },
     };
 
     // Options to control the language client
-    const clientOptions: LanguageClientOptions = {
+    const clientOptions: LC.LanguageClientOptions = {
         // Register the server for plain text documents
         documentSelector: [
             {
@@ -52,7 +48,7 @@ export function activate(context: ExtensionContext): void {
     };
 
     // Create the language client and start the client.
-    client = new LanguageClient("powerquery", "Power Query", serverOptions, clientOptions);
+    client = new LC.LanguageClient("powerquery", "Power Query", serverOptions, clientOptions);
 
     // Start the client. This will also launch the server
     client.start();
