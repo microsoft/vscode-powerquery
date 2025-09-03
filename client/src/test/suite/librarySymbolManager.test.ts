8c3faa0bf9aba9daaa1fbfc8ebe4b1090c241555
Matt Masson <mmasson@microsoft.com>
2025-09-03 19:19:33 -0400

    update node and typescript and dependencies

--- client/src/test/suite/librarySymbolManager.test.ts
+++ client/src/test/suite/librarySymbolManager.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as assert from "assert";
+import assert from "assert";
 import * as fs from "fs";
 import * as vscode from "vscode";
 
